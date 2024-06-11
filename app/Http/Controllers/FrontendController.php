699f39e47fadfa7c90adc79d099e526efeca9204
jesselizapaul <jesseliza.lotech@gmail.com>
2024-06-11 11:46:17 +0530

    home data get function

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Models\Banner;
 use App\Models\Aboutus;
+use App\Models\Business_Setup;
+use App\Models\Business__categories;
 use App\Models\Testimonials;
 use App\Models\Clients;
 use App\Models\EnquiryManagement;
@@ -17,11 +19,14 @@ class FrontendController extends Controller
     public function index()
     {
         $Banner = Banner::get();
-        $Aboutus = Aboutus::select('id', 'stitle_1', 'mtitle_1', 'description_1', 'image_1')->get();
+        $Aboutus = Aboutus::select('id', 'stitle_1', 'mtitle_1', 'description_1', 'image_1')->first();
+        $Business_Setup = Business_Setup::select('id', 'stitle', 'mtitle')->first();
+        $Business_categories = Business__categories::select('id', 'title', 'image', 'points_sub2')->take(3)->get();
+        $Service = service::select('id', 'logo', 'title', 'description')->take(3)->get();
         $Testimonials = Testimonials::get();
         $Clients = Clients::get();
-        $EnquiryManagement = EnquiryManagement::get();
-        $response = ['Banner' => $Banner, 'Aboutus' => $Aboutus, 'Testimonials' => $Testimonials, 'Clients' => $Clients, 'EnquiryManagement' => $EnquiryManagement];
+        $EnquiryManagement = EnquiryManagement::first();
+        $response = ['Banner' => $Banner, 'Aboutus' => $Aboutus, 'Business_Setup' => $Business_Setup, 'Business_categories', $Business_categories, 'Service' => $Service, 'Testimonials' => $Testimonials, 'Clients' => $Clients, 'EnquiryManagement' => $EnquiryManagement];
         return response()->json(['response' => $response, 'success' => true], JsonResponse::HTTP_OK);
     }
 
